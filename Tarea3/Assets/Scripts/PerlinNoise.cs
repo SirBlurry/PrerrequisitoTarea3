fe333b1b473285f3f7aaccb4f2bc6741d4c4b92b
SirBlurry <julian_master117@hotmail.com>
2020-03-04 20:38:00 -0600

    AddProcedural

---
+++ Tarea3/Assets/Scripts/PerlinNoise.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PerlinNoise : MonoBehaviour
+{
+    public int widht = 256;
+    public int height = 256;
+
+    public float scale = 20f;
+
+    public float offSetX = 100f;
+    public float offSetY = 100f;
+
+    private void Start()
+    {
+        offSetX = Random.Range(0f, 99999f);
+        offSetY = Random.Range(0f, 99999f);
+    }
+
+    private void Update()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = GenerateTexture();
+    }
+
+    private Texture2D GenerateTexture()
+    {
+        Texture2D texture = new Texture2D(widht, height);
+
+        for (int x = 0; x < widht; x++) 
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color color = CalculateColor(x, y);
+                texture.SetPixel(x, y, color);
+            }
+        }
+
+        texture.Apply();
+        return texture;
+    }
+
+    private Color CalculateColor(int x, int y)
+    {
+        float xCoord = (float)x / widht * scale + offSetX;
+        float yCoord = (float)y / height * scale + offSetY;
+
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        return new Color(sample, sample, sample);
+    }
+}
---
+++ Tarea3/Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    public int depth = 20;
+
+    public int widht = 256;
+    public int height = 256;
+
+    public float scale = 20f;
+
+    public float offSetX = 100f;
+    public float offSetY = 100f;
+
+    private void Start()
+    {
+        offSetX = Random.Range(0f, 9999f);
+        offSetY = Random.Range(0f, 9999f);
+    }
+
+    private void Update()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+
+        offSetX += Time.deltaTime * 5f;
+    }
+
+    private TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = widht + 1;
+
+        terrainData.size = new Vector3(widht, depth, height);
+
+        terrainData.SetHeights(0, 0, GenerateHeights());
+        return terrainData;
+    }
+
+    private float[,]GenerateHeights()
+    {
+        float[,] heights = new float[widht, height];
+        for (int x = 0; x < widht; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);
+            }
+        }
+
+        return heights;
+    }
+
+    private float CalculateHeight(int x, int y)
+    {
+        float xCoord = (float)x / widht * scale + offSetX;
+        float yCoord = (float)y / height * scale + offSetY;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
